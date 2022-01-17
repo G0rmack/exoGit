bb50011a428f3623f9b11a614ae9b7445d265f00
root <root@localhost.localdomain>
2022-01-17 07:41:00 -0500

    first commit

---
+++ exoGit/src/main/java/com/inti/exoGit/ExoGitApplication.java
@@ -0,0 +1,13 @@
+package com.inti.exoGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoGitApplication.class, args);
+	}
+
+}
---
+++ exoGit/src/test/java/com/inti/exoGit/ExoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.exoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
